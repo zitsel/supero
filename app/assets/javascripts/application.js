e9d066dd8ee00265934a221d4b570e63c0d0202e
Sterling James <sterling@revive-clothiers.com>
2014-02-20 21:24:47 +0000

    hopefully fixed image zoom

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,5 @@
 //= require jquery-fileupload/basic
 //= require jquery-fileupload/vendor/tmpl
 //= require twitter/bootstrap
-//= require twitter/bootstrap/bootstrap-popover.js
 //= require_tree .
 //= require colorbox-rails

bb6dcf3ce4570763b921cbc8e6a8939f1dc80f93
Sterling James <sterling@revive-clothiers.com>
2014-03-28 18:48:55 -0500

    removed examples

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,8 @@ set :repo_url, 'git@github.com:zitsel/supero.git'
 namespace :deploy do
   task :symlink_config do
     on roles(:web) do
-      execute "ln -nfs /usr/local/share/database.yml #{release_path}/config/database.yml"
-      execute "ln -nfs /usr/local/share/config.yml #{release_path}/config/config.yml"    
+      execute "ln -nfs /usr/local/www/share/database.yml #{release_path}/config/database.yml"
+      execute "ln -nfs /usr/local/www/share/config.yml #{release_path}/config/config.yml"    
     end
   end
 before "deploy:compile_assets", "deploy:symlink_config"

ce6ee2b3c849dcf386a6bb8d559df42ae9a42397
Sterling James <sterling@revive-clothiers.com>
2014-07-11 10:49:19 -0500

    fix hats model

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -39,11 +39,11 @@ class Hat < Product
   	"#{brand} #{description} #{size}"
   end
 
-  def self.sizes_col 
+  def sizes_col 
     ["6," "6 1/2", "6 3/4", "6 7/8", "7,", "7 1/8", "7 1/4", "7 3/8", "7 5/8", "8", "8 1/2", "XS", "S", "M", "L", "XL", "2XL", "One Size", "Adjustable", "7 1/2", "7 3/4", "7 7/8",].sort
   end
 
-  def self.styles_col
+  def styles_col
     ["Aviator/Trapper","Bandana","Baseball Cap","Beanie","Beret","Boonie/Bush","Bowler/Derby","Bucket","Cadet/Military","Cowboy/Western","Du-Rag/Biker","Fedora/Trilby","Newsboy/Cabbie","Panama","Russian Ushanka/Cossack","Ski","Top Hat","Trucker","Visor","Wide Brim"]
   end
  
